26818125712a3963eb43925b43a04d3768648b5c
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-02-23 10:33:19 +0100

    welcome page (working title)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        backgroundColor: "black",
+      }}
+    >
+      <h1 style={{ color: "white" }}>Working title: Mood Diary</h1>
+      <Link
+        style={{ color: "white" }}
+        href={"https://github.com/MathiSans/mood-diary"}
+      >
+        <small>Github</small>
+      </Link>
     </div>
   );
 }
