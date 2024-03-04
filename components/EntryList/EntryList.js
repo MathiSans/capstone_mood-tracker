1fc103bc1b3b374f8e34870f4e4c1e5742b16e50
Niko Cremer <n.ko@posteo.de>
2024-03-04 11:16:17 +0100

    refacture the Entry out of the EntryList

--- components/EntryList/EntryList.js
+++ components/EntryList/EntryList.js
@@ -1,44 +1,11 @@
-import { nanoid } from "nanoid";
-import Intensity from "@/utils/intensity";
-import {
-  EntryList,
-  Entry,
-  SliderText,
-  ExperienceText,
-  EntryText,
-  Separator,
-} from "./EntryList.styled";
+import Entry from "../Entry/Entry";
+import { EntryList } from "./EntryList.styled";
 
 export default function StyledEntryList({ allEntries }) {
   console.log("styled entrylist", allEntries);
   return (
     <EntryList>
-      {allEntries.map((entry) => {
-        return (
-          <Entry key={entry.id}>
-            <small>{entry.date}</small>
-            <p>
-              You felt{" "}
-              <SliderText>
-                <Intensity intensity={entry.slider} />.
-              </SliderText>{" "}
-              You selected these tags:{" "}
-              <ExperienceText>
-                {entry.experiences
-                  .filter((experience) => Object.values(experience)[0])
-                  .map((experience, index, array) => (
-                    <span key={nanoid()}>
-                      {Object.keys(experience)[0]}
-                      {index < array.length - 1 && ", "}
-                    </span>
-                  ))}
-              </ExperienceText>
-              <span>. You wrote:</span> <EntryText>{entry.text}</EntryText>
-            </p>
-            <Separator />
-          </Entry>
-        );
-      })}
+      <Entry allEntries={allEntries} />
     </EntryList>
   );
 }
--- components/EntryList/EntryList.styled.js
+++ components/EntryList/EntryList.styled.js
@@ -4,26 +4,3 @@ export const EntryList = styled.ul`
   list-style-type: none;
   padding: 0;
 `;
-export const Entry = styled.li`
-  display: flex;
-  justify-content: start;
-  flex-direction: column;
-`;
-
-export const SliderText = styled.span`
-  font-weight: bold;
-`;
-
-export const ExperienceText = styled.span`
-  font-weight: bold;
-`;
-
-export const EntryText = styled.span`
-  font-weight: bold;
-  font-style: italic;
-`;
-
-export const Separator = styled.hr`
-  border: 0.5px solid grey;
-  width: 100%;
-`;
