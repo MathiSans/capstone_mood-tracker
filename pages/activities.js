f8742c962c66bf7ef1b06edfa968879e131f38e1
Niko Cremer <n.ko@posteo.de>
2024-03-18 22:07:20 +0100

    select the emotion and console log it with a handle function

--- pages/activities.js
+++ pages/activities.js
@@ -3,11 +3,38 @@ import ActivityList from "@/components/ActivityList/ActivityList";
 import styled from "styled-components";
 
 export default function ActivityListRender() {
+  function handleChangeOfEmotion(event) {
+    const selectedEmotion = event.target.value;
+    if (selectedEmotion == "enjoyment") {
+      console.log("YUHUU FIDELDII🕺🏻🥳");
+    } else {
+      console.log(event.target.value);
+    }
+  }
   return (
     <>
       <TrackerContainer>
         <h1>
-          List of activites for <StyledYourMood>*Your Mood*</StyledYourMood>
+          <StyledHeadlineInline>
+            <StyledYourMood> List of activites for</StyledYourMood>
+            <form>
+              <label>
+                <StyledSelect
+                  name="emotions"
+                  size="5"
+                  onChange={(event) => {
+                    handleChangeOfEmotion(event);
+                  }}
+                >
+                  <option value={"fear"}>fear</option>
+                  <option value={"anger"}>anger</option>
+                  <option value={"enjoyment"}>enjoyment</option>
+                  <option value={"disgust"}>disgust</option>
+                  <option value={"sadness"}>sadness</option>
+                </StyledSelect>
+              </label>
+            </form>
+          </StyledHeadlineInline>
         </h1>
         <ActivityList />
       </TrackerContainer>
@@ -20,4 +47,18 @@ const StyledYourMood = styled.span`
   background: -webkit-linear-gradient(#cdea80, #cdeaff);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+`;
+
+const StyledSelect = styled.select`
+  color: black;
+  padding: 0.2rem;
+`;
+
+const StyledHeadlineInline = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &:active {
+    border: 0;
+  }
 `;
