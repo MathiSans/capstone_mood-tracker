9d6ec7d177632b2162d4abf4ab4f814d3578f51b
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-03-20 14:23:13 +0100

    changed location json provider

--- utils/locationTracking.js
+++ utils/locationTracking.js
@@ -1,11 +1,11 @@
 export default async function fetchLocation() {
   try {
-    const response = await fetch("http://ip-api.com/json/");
+    const response = await fetch("https://ipinfo.io/json");
     if (!response.ok) {
       throw new Error("Failed to fetch location information");
     }
     const data = await response.json();
-    const region = data.regionName || "unknown";
+    const region = data.region || "unknown";
     return region;
   } catch (error) {
     console.error("Error fetching location:", error.message);
