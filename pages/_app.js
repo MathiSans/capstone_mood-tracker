2c2b2a33e9e8e6ecb95660e99de6d5dc7b8640d6
Niko Cremer <n.ko@posteo.de>
2024-03-22 19:56:38 +0100

    Dark-light-theme Toogle works partly

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,38 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
+import { ThemeProvider } from "styled-components";
+//import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+
+const darkTheme = {
+  color: "white",
+  backgroundColor: "black",
+};
+const lightTheme = {
+  color: "black",
+  backgroundColor: "white",
+};
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useLocalStorageState("dark");
+  const isDarkTheme = theme === "dark";
+
+  const handleToggleTheme = () => {
+    setTheme(isDarkTheme ? "light" : "dark");
+  };
   return (
     <>
-      <GlobalStyle />
-      <SWRConfig
-        value={{
-          fetcher: (resource, init) =>
-            fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
+      <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <Component {...pageProps} handleToggleTheme={handleToggleTheme} />
+        </SWRConfig>
+      </ThemeProvider>
     </>
   );
 }
---
+++ pages/theme.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+export default function ThemeTest({ handleToggleTheme }) {
+  const [isToggled, setIsToggled] = useState(false);
+
+  //   const onToggle = () => {
+  //     setIsToggled(!isToggled);
+  //     handleToggleTheme();
+  //   };
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Box>
+        <p>Box</p>
+      </Box>
+      <button
+        onClick={() => {
+          handleToggleTheme();
+        }}
+      ></button>
+    </>
+  );
+}
+
+const Box = styled.div`
+  background-color: ${(prop) => prop.theme.backgroundColor};
+  color: ${(prop) => prop.theme.color};
+`;
