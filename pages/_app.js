bfe5b0ddabe2d4b4a5e4940f4664825e56a8dd6d
Niko Cremer <n.ko@posteo.de>
2024-03-27 13:14:32 +0100

    adding comments

--- pages/_app.js
+++ pages/_app.js
@@ -9,29 +9,32 @@ const theme = {
     light: "white",
     tertiary: "grey",
     danger: "red",
-    success: "#4caf50",
   },
   fonts: {
     body: "system-ui",
     controls: "monospace",
   },
   breakpoints: {
+    //just examples
     small: "576px",
     medium: "768px",
     large: "992px",
     xlarge: "1200px",
   },
   spacing: {
+    //just examples
     small: "8px",
     medium: "16px",
     large: "24px",
   },
   effects: {
+    //just examples
     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
     linearGradient: "linear-gradient(to right, #ff416c, #ff4b2b)",
     radialGradient: "radial-gradient(circle, #00ff00, #0000ff)",
   },
   borders: {
+    //just examples
     strength: "2px",
     color: "#333333",
   },
