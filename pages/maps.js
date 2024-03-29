19f93542a63960374b7cba6d2674c655441e4568
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-03-29 17:20:40 +0100

    re-import analyser functions

--- pages/maps.js
+++ pages/maps.js
@@ -2,8 +2,8 @@ import { Container } from "@/components/Layout/Layout.styled";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import useSWR from "swr";
-import locationAnalyser from "@/utils/locationAnalyser";
 import experienceAnalyser from "@/utils/experienceAnalyser";
+import locationAnalyser from "@/utils/locationAnalyser";
 import { useState } from "react";
 import MapOfCircles from "@/components/MapOfCircles/MapOfCircles";
 
