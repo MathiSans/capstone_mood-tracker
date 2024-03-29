f3cf98286a73ad3f922ef42c3ad39a226c002d0d
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-03-29 17:44:24 +0100

    renamed, will it work now?

--- pages/maps.js
+++ pages/maps.js
@@ -2,8 +2,8 @@ import { Container } from "@/components/Layout/Layout.styled";
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import useSWR from "swr";
-import locationAnalyser from "@/utils/IUHUHIUlocationAnalyser.js";
-import experienceAnalyser from "@/utils/HIHIHexperienceAnalyser.js";
+import locationAnalyser from "@/utils/locationAnalyser.js";
+import experienceAnalyser from "@/utils/experienceAnalyser.js";
 import { useState } from "react";
 import MapOfCircles from "@/components/MapOfCircles/MapOfCircles";
 
