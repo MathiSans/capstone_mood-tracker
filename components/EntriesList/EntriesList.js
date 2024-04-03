f9dcf31d21bc71dbb4b30949d4bd3e13e0aca17c
Niko Cremer <n.ko@posteo.de>
2024-04-03 23:48:23 +0200

    add moin to option value 3

--- components/EntriesList/EntriesList.js
+++ components/EntriesList/EntriesList.js
@@ -5,46 +5,12 @@ import { AnimatePresence, motion } from "framer-motion";
 import { FiTrash2 } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Circle from "../Circle/Circle";
 
-export default function EntriesList({ data }) {
-  const [filtered, setFiltered] = useState([]);
+export default function EntriesList({ filtered, filter }) {
   const router = useRouter();
   const [deletingId, setDeletingId] = useState(null);
   const { mutate } = useSWRConfig();
-  useEffect(() => {
-    console.log("Data received:", data);
-    const currentDate = new Date();
-    const reversedData = [...data].reverse();
-    console.log("Reversed data:", reversedData);
-
-    // Create an array to store the last 7 days' dates
-    const lastSevenDays = [];
-
-    // Loop through the last 7 days and push their formatted dates into the array
-    for (let i = 0; i < 7; i++) {
-      const date = new Date(currentDate.getTime() - i * 24 * 60 * 60 * 1000);
-      const formattedDate1 = `${date.getDate()}/${
-        date.getMonth() + 1
-      }/${date.getFullYear()}`;
-      const formattedDate2 = `${date.getDate()}.${
-        date.getMonth() + 1
-      }.${date.getFullYear()}`;
-      lastSevenDays.push(`${formattedDate1}, ${formattedDate2}`);
-    }
-    console.log("Last seven days:", lastSevenDays);
-    const lastSevenDaysEntries = reversedData.filter((entry) => {
-      const entryDate = entry.time.split(",")[0].trim(); // Get the date part of the entry's time
-      const match = lastSevenDays.some((dates) => {
-        const [date1, date2] = dates.split(",").map((date) => date.trim()); // Split the dates in lastSevenDays array
-        return entryDate === date1 || entryDate === date2; // Check for a match
-      });
-      return match;
-    });
-
-    console.log("Last seven days entries:", lastSevenDaysEntries);
-
-    setFiltered(lastSevenDaysEntries);
-  }, [data]);
 
   function handleDeleteDialog(event, id) {
     event.stopPropagation();
@@ -61,74 +27,80 @@ export default function EntriesList({ data }) {
 
   return (
     <>
-      <Styled.Grid>
-        <AnimatePresence>
-          {filtered.map((entry) => (
-            <motion.div
-              key={entry._id}
-              whileHover={{ scale: 1.05 }}
-              exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.3 } }}
-            >
-              <Styled.Card onClick={() => router.push(`${entry._id}`)}>
-                <Styled.AnimationContainer>
-                  <Styled.ColoredShape color={entry.color} />
-                </Styled.AnimationContainer>
-                <Styled.Sentence>
-                  <Styled.StaticText>Somebody </Styled.StaticText>
-                  {entry.location === "unknown" ? "" : `in ${entry.location}`}
-                  <Styled.StaticText> felt</Styled.StaticText>{" "}
-                  {entry.experience}.{" "}
-                  <Styled.StaticText>More specifically</Styled.StaticText>{" "}
-                  <Intensity
-                    value={entry.intensity}
-                    experience={entry.experience}
-                  />
-                  <Styled.StaticText>
-                    . They selected these tags:
-                  </Styled.StaticText>{" "}
-                  {entry.reactions.map((reaction, index, array) => (
-                    <span key={index}>
-                      {reaction}
-                      {index < array.length - 1 && ", "}
-                    </span>
-                  ))}
-                </Styled.Sentence>
-                <Styled.ButtonContainer>
-                  {deletingId === entry._id ? (
-                    <>
-                      <Styled.DeleteQuestion>
-                        sure about deleting?
-                      </Styled.DeleteQuestion>
-                      <Styled.DeleteAnswer
-                        red
-                        onClick={(event) => handleDeleteEntry(event, entry._id)}
-                      >
-                        yes
-                      </Styled.DeleteAnswer>
-                      <Styled.DeleteAnswer
-                        onClick={(event) => handleDeleteDialog(event, null)}
-                      >
-                        no
-                      </Styled.DeleteAnswer>
-                    </>
-                  ) : (
-                    <>
-                      <Styled.RoundButton
-                        as="a"
-                        onClick={(event) =>
-                          handleDeleteDialog(event, entry._id)
-                        }
-                      >
-                        <FiTrash2 />
-                      </Styled.RoundButton>
-                    </>
-                  )}
-                </Styled.ButtonContainer>
-              </Styled.Card>
-            </motion.div>
-          ))}
-        </AnimatePresence>
-      </Styled.Grid>
+      {filter == "value 3" ? (
+        <h1>Moin!</h1>
+      ) : (
+        <Styled.Grid>
+          <AnimatePresence>
+            {filtered.map((entry) => (
+              <motion.div
+                key={entry._id}
+                whileHover={{ scale: 1.05 }}
+                exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.3 } }}
+              >
+                <Styled.Card onClick={() => router.push(`${entry._id}`)}>
+                  <Styled.AnimationContainer>
+                    <Styled.ColoredShape color={entry.color} />
+                  </Styled.AnimationContainer>
+                  <Styled.Sentence>
+                    <Styled.StaticText>Somebody </Styled.StaticText>
+                    {entry.location === "unknown" ? "" : `in ${entry.location}`}
+                    <Styled.StaticText> felt</Styled.StaticText>{" "}
+                    {entry.experience}.{" "}
+                    <Styled.StaticText>More specifically</Styled.StaticText>{" "}
+                    <Intensity
+                      value={entry.intensity}
+                      experience={entry.experience}
+                    />
+                    <Styled.StaticText>
+                      . They selected these tags:
+                    </Styled.StaticText>{" "}
+                    {entry.reactions.map((reaction, index, array) => (
+                      <span key={index}>
+                        {reaction}
+                        {index < array.length - 1 && ", "}
+                      </span>
+                    ))}
+                  </Styled.Sentence>
+                  <Styled.ButtonContainer>
+                    {deletingId === entry._id ? (
+                      <>
+                        <Styled.DeleteQuestion>
+                          sure about deleting?
+                        </Styled.DeleteQuestion>
+                        <Styled.DeleteAnswer
+                          red
+                          onClick={(event) =>
+                            handleDeleteEntry(event, entry._id)
+                          }
+                        >
+                          yes
+                        </Styled.DeleteAnswer>
+                        <Styled.DeleteAnswer
+                          onClick={(event) => handleDeleteDialog(event, null)}
+                        >
+                          no
+                        </Styled.DeleteAnswer>
+                      </>
+                    ) : (
+                      <>
+                        <Styled.RoundButton
+                          as="a"
+                          onClick={(event) =>
+                            handleDeleteDialog(event, entry._id)
+                          }
+                        >
+                          <FiTrash2 />
+                        </Styled.RoundButton>
+                      </>
+                    )}
+                  </Styled.ButtonContainer>
+                </Styled.Card>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </Styled.Grid>
+      )}
     </>
   );
 }
--- pages/entries.js
+++ pages/entries.js
@@ -2,12 +2,54 @@ import React from "react";
 import Guide from "@/components/Guide/Guide";
 import EntriesList from "@/components/EntriesList/EntriesList";
 import { Container, Navigation, Page } from "@/components/Layout/Layout.styled";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 export default function Entries() {
   const { data, isLoading } = useSWR("/api/entries");
   const [filter, setFilter] = useState(""); //Filter Entries State
+  const [filtered, setFiltered] = useState([]);
+
+  console.log("Data received:", data);
+
+  useEffect(() => {
+    if (Array.isArray(data)) {
+      const currentDate = new Date();
+      const reversedData = [...data].reverse();
+      console.log("Reversed data:", reversedData);
+      // Create an array to store the last 7 days' dates
+      const lastSevenDays = [];
+
+      // Loop through the last 7 days and push their formatted dates into the array
+      for (let i = 0; i < 7; i++) {
+        const date = new Date(currentDate.getTime() - i * 24 * 60 * 60 * 1000);
+        const formattedDate1 = `${date.getDate()}/${
+          date.getMonth() + 1
+        }/${date.getFullYear()}`;
+        const formattedDate2 = `${date.getDate()}.${
+          date.getMonth() + 1
+        }.${date.getFullYear()}`;
+        lastSevenDays.push(`${formattedDate1}, ${formattedDate2}`);
+      }
+      console.log("Last seven days:", lastSevenDays);
+      if (filter === "value 2") {
+        const lastSevenDaysEntries = reversedData.filter((entry) => {
+          const entryDate = entry.time.split(",")[0].trim(); // Get the date part of the entry's time
+          const match = lastSevenDays.some((dates) => {
+            const [date1, date2] = dates.split(",").map((date) => date.trim()); // Split the dates in lastSevenDays array
+            return entryDate === date1 || entryDate === date2; // Check for a match
+          });
+          return match;
+        });
+        console.log("Last seven days entries:", lastSevenDaysEntries);
+        setFiltered(lastSevenDaysEntries);
+      } else {
+        setFiltered(reversedData); // Set filtered to reversedData for other cases
+      }
+    } else {
+      console.error("Data is not iterable (not an array)");
+    }
+  }, [data, filter]);
 
   if (isLoading) {
     return <p>loading...</p>;
@@ -17,31 +59,10 @@ export default function Entries() {
     return;
   }
 
-  //Filter Entries Code
-  // Function to filter entries based on the last 7 days
-  function filterLast7Days(entry) {
-    const today = new Date(); // Current date
-    const sevenDaysAgo = new Date(today);
-    sevenDaysAgo.setDate(today.getDate() - 7); // Subtract 7 days
-
-    const entryDate = new Date(entry.date_created);
-    return entryDate >= sevenDaysAgo && entryDate <= today;
-  }
-
-  // Filter entries
-  const filteredEntries = data.filter(filterLast7Days);
-
-  // Print the filtered entries
-  filteredEntries.forEach((entry) => {
-    console.log(entry.date_created);
-  });
-
   function handleEntryFilter(event) {
     if (event.target.value === "1") {
-      data.filter((week) => {
-        week.date;
-      });
       setFilter("value 1");
+      setFiltered([...data].reverse());
     }
     if (event.target.value === "2") {
       setFilter("value 2");
@@ -58,11 +79,11 @@ export default function Entries() {
       <Page>
         <Guide text={"emotion collection"} />
         <select onChange={handleEntryFilter}>
-          <option value="2">2</option>
-          <option value="1">Show Emotion of the Last Week</option>
-          <option value="3">3</option>
+          <option value="1">Show All</option>
+          <option value="2">Show Emotion of the Last Week</option>
+          <option value="3">Visualize</option>
         </select>
-        <EntriesList data={data} />
+        <EntriesList filtered={filtered} filter={filter} />
       </Page>
     </Container>
   );
