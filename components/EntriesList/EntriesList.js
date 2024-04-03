fe5f08dbc3956736edbfb222c1cc7e8eda488042
Niko Cremer <n.ko@posteo.de>
2024-04-03 23:18:22 +0200

    show only the entries of the last week

--- components/EntriesList/EntriesList.js
+++ components/EntriesList/EntriesList.js
@@ -3,13 +3,48 @@ import * as Styled from "./EntriesList.styled";
 import { useSWRConfig } from "swr";
 import { AnimatePresence, motion } from "framer-motion";
 import { FiTrash2 } from "react-icons/fi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function EntriesList({ data }) {
+  const [filtered, setFiltered] = useState([]);
   const router = useRouter();
   const [deletingId, setDeletingId] = useState(null);
   const { mutate } = useSWRConfig();
+  useEffect(() => {
+    console.log("Data received:", data);
+    const currentDate = new Date();
+    const reversedData = [...data].reverse();
+    console.log("Reversed data:", reversedData);
+
+    // Create an array to store the last 7 days' dates
+    const lastSevenDays = [];
+
+    // Loop through the last 7 days and push their formatted dates into the array
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(currentDate.getTime() - i * 24 * 60 * 60 * 1000);
+      const formattedDate1 = `${date.getDate()}/${
+        date.getMonth() + 1
+      }/${date.getFullYear()}`;
+      const formattedDate2 = `${date.getDate()}.${
+        date.getMonth() + 1
+      }.${date.getFullYear()}`;
+      lastSevenDays.push(`${formattedDate1}, ${formattedDate2}`);
+    }
+    console.log("Last seven days:", lastSevenDays);
+    const lastSevenDaysEntries = reversedData.filter((entry) => {
+      const entryDate = entry.time.split(",")[0].trim(); // Get the date part of the entry's time
+      const match = lastSevenDays.some((dates) => {
+        const [date1, date2] = dates.split(",").map((date) => date.trim()); // Split the dates in lastSevenDays array
+        return entryDate === date1 || entryDate === date2; // Check for a match
+      });
+      return match;
+    });
+
+    console.log("Last seven days entries:", lastSevenDaysEntries);
+
+    setFiltered(lastSevenDaysEntries);
+  }, [data]);
 
   function handleDeleteDialog(event, id) {
     event.stopPropagation();
@@ -24,13 +59,11 @@ export default function EntriesList({ data }) {
     mutate("/api/entries");
   }
 
-  const reversedMoods = data.slice().reverse();
-
   return (
     <>
       <Styled.Grid>
         <AnimatePresence>
-          {reversedMoods.map((entry) => (
+          {filtered.map((entry) => (
             <motion.div
               key={entry._id}
               whileHover={{ scale: 1.05 }}
