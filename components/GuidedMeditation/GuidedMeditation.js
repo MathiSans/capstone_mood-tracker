66d6dfdff3c79c877a72caa348781a86a79a06b4
Niko Cremer <n.ko@posteo.de>
2024-04-10 00:13:47 +0200

    clean up code

--- components/GuidedMeditation/GuidedMeditation.js
+++ components/GuidedMeditation/GuidedMeditation.js
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 export default function GuidedMeditation() {
   return (
     <div>
--- pages/mindful.js
+++ pages/mindful.js
@@ -2,7 +2,6 @@ import React from "react";
 import Guide from "@/components/Guide/Guide";
 import NavButton from "@/components/NavButton/NavButton";
 import { useRouter } from "next/router";
-import * as Styled from "@/components/Layout/Layout";
 import GuidedMeditation from "@/components/GuidedMeditation/GuidedMeditation";
 
 export default function Entries() {
