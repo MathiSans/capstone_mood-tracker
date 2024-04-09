97310c1b67ac2691a28517fc41ae897dface6fc1
Sevket Ersan <sevketersan@hotmail.com>
2024-04-09 13:02:52 +0200

    background color var

--- components/3DAnimation/3DAnimation.styled.js
+++ components/3DAnimation/3DAnimation.styled.js
@@ -4,10 +4,11 @@ import { Canvas } from "@react-three/fiber";
 export const Container = styled.div`
   position: absolute;
   z-index: -1;
+  background: var(--color-main);
   width: 100%;
   height: 100%;
 `;
 
 export const CustomCanvas = styled(Canvas)`
-  background-color: black;
+  background-color: var(--color-main);
 `;
