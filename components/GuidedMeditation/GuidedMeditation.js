a3c40167e33182e7332d3490f6544dc77b2743fb
Niko Cremer <n.ko@posteo.de>
2024-04-10 14:12:05 +0200

    fix code

--- components/GuidedMeditation/GuidedMeditation.js
+++ components/GuidedMeditation/GuidedMeditation.js
@@ -6,39 +6,35 @@ import "react-h5-audio-player/lib/styles.css";
 export default function GuidedMeditation() {
   return (
     <>
-      <div>
-        <h3>Meditation: Living Presence – with Body Scan (12:06 min.)</h3>
+      <MeditationContainer>
+        <TextContainer>
+          <h3>Meditation: Living Presence – with Body Scan (12:06 min.)</h3>
 
-        <article>
-          A key pathway to full presence is awakening through the body. This
-          meditation guides us through a body scan, relaxing and receiving the
-          play of sensations. We then deepen attention to the breath, and rest
-          with the rhythmic waves of breathing, experiencing the background of
-          the whole domain of physical aliveness.
-        </article>
-        <br />
+          <Article>
+            A key pathway to full presence is awakening through the body. This
+            meditation guides us through a body scan, relaxing and receiving the
+            play of sensations. We then deepen attention to the breath, and rest
+            with the rhythmic waves of breathing, experiencing the background of
+            the whole domain of physical aliveness.
+          </Article>
+        </TextContainer>
 
         <AudioPlayer
-          autoPlay
           src="https://traffic.libsyn.com/tarabrach/2023-10-11-Meditation-Living-Presence-with-Body-Scan-2016-05-25-TaraBrach.mp3?_=1"
           onPlay={(e) => console.log("onPlay")}
+
           // other props here
         />
 
-        <br />
-        <br />
-
-        <small>
+        <SourceContainer>
           source: Meditation: Living Presence – with Body Scan (12:06 min.) Oct
           11, 2023;
           <br />
           <BrightLink href="https://www.tarabrach.com/meditation-living-presence-body-scan-2/">
             https://www.tarabrach.com/meditation-living-presence-body-scan-2/
           </BrightLink>
-        </small>
-        <br />
-        <br />
-      </div>
+        </SourceContainer>
+      </MeditationContainer>
     </>
   );
 }
@@ -51,4 +47,23 @@ const BrightLink = styled(Link)`
   &:hover {
     text-decoration: underline; /* Underline on hover (you can modify this effect) */
   }
+`;
+
+const TextContainer = styled.div`
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+`;
+
+const SourceContainer = styled.div`
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+  font-size: small;
+`;
+
+const Article = styled.article`
+  margin-top: 1%.5;
+`;
+
+const MeditationContainer = styled.div`
+  padding: 0 20rem 0 20rem;
 `;
--- pages/mindful.js
+++ pages/mindful.js
@@ -3,6 +3,7 @@ import Guide from "@/components/Guide/Guide";
 import NavButton from "@/components/NavButton/NavButton";
 import { useRouter } from "next/router";
 import GuidedMeditation from "@/components/GuidedMeditation/GuidedMeditation";
+import styled from "styled-components";
 
 export default function Entries() {
   const router = useRouter();
@@ -11,8 +12,13 @@ export default function Entries() {
     <>
       <Guide text={"Guided Meditation"} />
       <GuidedMeditation />
-
-      <NavButton handleClick={() => router.back}>back</NavButton>
+      <ButtonContainer>
+        <NavButton handleClick={() => router.back}>back</NavButton>
+      </ButtonContainer>
     </>
   );
 }
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
