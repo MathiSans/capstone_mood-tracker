c252e95d8ea2ef7143a383657041383ad1efb027
Niko Cremer <n.ko@posteo.de>
2024-04-10 08:56:59 +0200

    delete old code

--- components/GuidedMeditation/GuidedMeditation.js
+++ components/GuidedMeditation/GuidedMeditation.js
@@ -24,16 +24,7 @@ export default function GuidedMeditation() {
           onPlay={(e) => console.log("onPlay")}
           // other props here
         />
-        {/* <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube-nocookie.com/embed/iHGj23psLNY?si=jhMt3-acuO-B3lBu&amp;controls=0"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe> */}
+
         <br />
         <br />
 
