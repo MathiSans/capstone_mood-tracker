a1977a6eb895ae262c249407037be0986e0d8d82
Niko Cremer <n.ko@posteo.de>
2024-04-12 13:43:47 +0200

    added it to the tool measurement

--- pages/tools/[tool].js
+++ pages/tools/[tool].js
@@ -4,13 +4,14 @@ import { useRouter } from "next/router";
 import QuotesWrapper from "../../components/QuotesWrapper/QuotesWrapper";
 import SmileTrainerWrapper from "@/components/SmileTrainerWrapper/SmileTrainerWrapper";
 import { Container, Page } from "@/components/Layout/Layout.styled";
+import GuidedMeditationWrapper from "@/components/GuidedMeditationWrapper/GuidedMeditationWrapper";
 
 export default function SingleToolPage() {
   const [page, setPage] = useState(0);
   const router = useRouter();
   const { tool } = router.query;
 
-  const links = ["quotes", "smiletrainer"];
+  const links = ["quotes", "smiletrainer", "meditation"];
 
   return (
     <>
@@ -23,6 +24,7 @@ export default function SingleToolPage() {
               <ToolMoodMeasurer page={page} setPage={setPage} />
               {page === 1 && tool === links[0] && <QuotesWrapper />}
               {page === 1 && tool === links[1] && <SmileTrainerWrapper />}
+              {page === 1 && tool === links[2] && <GuidedMeditationWrapper />}
             </>
           )}
         </Page>
