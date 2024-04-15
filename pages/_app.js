877f78f212df52afd5703533a3241639341ebda8
Ramin Faqyri <ramin-faqyri@web.de>
2024-04-15 10:29:28 +0200

    put const theme into styles.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,54 +2,8 @@ import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
 import Overlay from "@/components/Overlay/Overlay";
 import { ThemeProvider } from "styled-components";
+import { theme } from "../styles.js";
 import { SessionProvider } from "next-auth/react";
-
-const theme = {
-  colors: {
-    dark: "black",
-    light: "white",
-    neutral: "grey",
-    danger: "red",
-  },
-  fonts: {
-    main: "system-ui",
-    serif: "Cambria, Cochin, Georgia, Times, serif",
-  },
-  fontSize: {
-    default: "1rem",
-    small: "0.8rem",
-    large: "2rem",
-    xl: "3rem",
-  },
-  fontWeight: {
-    light: "200",
-    normal: "400",
-    bold: "700",
-  },
-  spacing: {
-    xs: "4px",
-    s: "8px",
-    m: "12px",
-    l: "20px",
-    xl: "28px",
-    xxl: "32px",
-    xxxl: "44px",
-  },
-  effects: {
-    boxShadow: "",
-    dropShadow: "",
-    linearGradient: "-webkit-linear-gradient(#e3f710, #ff0000)",
-    radialGradient:
-      "radial-gradient(circle, rgba(42, 42, 42, 1) 0%, rgba(13, 13, 13, 1) 100%)",
-  },
-  borders: {
-    radiusSmall: "12px",
-    radiusMedium: "20px",
-    radiusLarge: "100px",
-    radiusRound: "50%",
-    strength: "2px",
-  },
-};
 
 export default function App({
   Component,
--- styles.js
+++ styles.js
@@ -1,5 +1,52 @@
 import { createGlobalStyle } from "styled-components";
 
+export const theme = {
+  colors: {
+    dark: "black",
+    light: "white",
+    neutral: "grey",
+    danger: "red",
+  },
+  fonts: {
+    main: "system-ui",
+    serif: "Cambria, Cochin, Georgia, Times, serif",
+  },
+  fontSize: {
+    default: "1rem",
+    small: "0.8rem",
+    large: "2rem",
+    xl: "3rem",
+  },
+  fontWeight: {
+    light: "200",
+    normal: "400",
+    bold: "700",
+  },
+  spacing: {
+    xs: "4px",
+    s: "8px",
+    m: "12px",
+    l: "20px",
+    xl: "28px",
+    xxl: "32px",
+    xxxl: "44px",
+  },
+  effects: {
+    boxShadow: "",
+    dropShadow: "",
+    linearGradient: "-webkit-linear-gradient(#e3f710, #ff0000)",
+    radialGradient:
+      "radial-gradient(circle, rgba(42, 42, 42, 1) 0%, rgba(13, 13, 13, 1) 100%)",
+  },
+  borders: {
+    radiusSmall: "12px",
+    radiusMedium: "20px",
+    radiusLarge: "100px",
+    radiusRound: "50%",
+    strength: "2px",
+  },
+};
+
 export default createGlobalStyle`
   *,
   *::before,
