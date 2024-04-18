d4838e1ae04ff7353faca57ca4d904a0faba54f1
Ramin Faqyri <ramin-faqyri@web.de>
2024-04-18 13:03:56 +0200

    created index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,51 @@
-import Flow from "@/components/Flow/Flow";
+import { useState } from "react";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export default function HomePage() {
-  return <Flow />;
+export default function Home() {
+  const router = useRouter();
+  const { page } = router.query;
+  const [dashboardIsOpen, setDashboardIsOpen] = useState(false);
+  const [audioTrigger, setAudioTrigger] = useState(false);
+  const { data: allEntries } = useSWR("/api/entries");
+  const { data: session } = useSession();
+  const { data: allActivities } = useSWR("/api/activities");
+  const [anonymousFetchedData, setAnonymousFetchedData] = useState({
+    allEntries: [],
+    lastWeekEntries: [],
+    allActivities: [],
+  });
+  const [userFetchedData, setUserFetchedData] = useState({
+    userEntries: [],
+    userLastWeekEntries: [],
+    allActivities: [],
+  });
+
+  return (
+    <>
+      {!links.includes(tool) ? (
+        <p>(404) page not found</p>
+      ) : (
+        <>
+          {}
+          <ActionBar />
+          {dashboardIsOpen && <Dashboard />}
+          <ToolMoodMeasurer page={page} setPage={setPage} />
+          {page === "smiletrainer" && <QuotesWrapper />}
+          {page === "newentry" && <EntryFlow />}
+          {page === "singleentry" && <SingleEntry />}
+          {page === "tool" && <ToolWrapper />}
+        </>
+      )}
+    </>
+  );
 }
+
+// props: session from _app.js
+// states: dashboardIsOpen, audioTrigger
+// anonymousFetchedData (nicht eingeloggt): allEntries, lastWeekEntries, AllActivities
+// userFetchedData (eingeloggt): userEntries, userLastWeekEntries, AllActivities
+// functions: fetchData
+//        handleDashboardIsOpen()
+// Page Routing: const { page } = router.query; (https://mood-tracker.com/smiletrainer)
---
+++ pages/index_v1.js
@@ -0,0 +1,5 @@
+import Flow from "@/components/Flow/Flow";
+
+export default function HomePage() {
+  return <Flow />;
+}
