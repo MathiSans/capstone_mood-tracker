36072da4952405fbf89dda58e989c169943c19d9
Mathis Santos <mail@mathissantos.com>
2024-04-18 19:05:19 +0200

    added useEffect to fetch all user entries and activities, as well as anonymous entries/activities

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-  const { page } = router.query;
-  const [dashboardIsOpen, setDashboardIsOpen] = useState(false);
-  const [audioTrigger, setAudioTrigger] = useState(false);
+  const { URL } = router.query;
   const { data: allEntries } = useSWR("/api/entries");
-  const { data: session } = useSession();
   const { data: allActivities } = useSWR("/api/activities");
-  const [anonymousFetchedData, setAnonymousFetchedData] = useState({
-    allEntries: [],
-    lastWeekEntries: [],
-    allActivities: [],
-  });
-  const [userFetchedData, setUserFetchedData] = useState({
-    userEntries: [],
-    userLastWeekEntries: [],
-    allActivities: [],
-  });
+  const { data: session } = useSession();
+  const [audioTrigger, setAudioTrigger] = useState(false);
+  const [dashboardIsOpen, setDashboardIsOpen] = useState(false);
+  const [anonymousFetchedData, setAnonymousFetchedData] = useState({});
+  const [userFetchedData, setUserFetchedData] = useState({});
+
+  useEffect(() => {
+    if (allEntries && session && allActivities) {
+      const userEntries = allEntries.filter(
+        (entry) => entry.user === session.user.id
+      );
+      setUserFetchedData((prevState) => ({
+        ...prevState,
+        userEntries,
+        allActivities,
+      }));
+      setAnonymousFetchedData((prevState) => ({
+        ...prevState,
+        allEntries,
+        allActivities,
+      }));
+    }
+  }, [allEntries, session, allActivities]);
+
+  console.log("User:", userFetchedData);
+  console.log("Anon:", anonymousFetchedData);
+
+  function handleDashboardIsOpen() {
+    setDashboardIsOpen(!dashboardIsOpen);
+  }
 
   return (
     <>
-      {!links.includes(tool) ? (
-        <p>(404) page not found</p>
-      ) : (
-        <>
-          {}
-          <ActionBar />
-          {dashboardIsOpen && <Dashboard />}
-          <ToolMoodMeasurer page={page} setPage={setPage} />
-          {page === "smiletrainer" && <QuotesWrapper />}
-          {page === "newentry" && <EntryFlow />}
-          {page === "singleentry" && <SingleEntry />}
-          {page === "tool" && <ToolWrapper />}
-        </>
-      )}
+      {/* <ActionBar
+        session={session}
+        handleDashboardIsOpen={handleDashboardIsOpen}
+        dashboardIsOpen={dashboardIsOpen}
+      />
+      {dashboardIsOpen && (
+        <Dashboard
+          fetchData={session ? userFetchedData : anonymousFetchedData}
+        />
+      )} */}
+      {/* {URL.includes("smiletrainer") && <QuotesWrapper />}
+      {URL.includes("newentry") && <EntryFlow />}
+      {URL.includes("singleentry") && <SingleEntry />}
+      {URL.includes("activities") && <ToolWrapper />} */}
     </>
   );
 }
 
-// props: session from _app.js
-// states: dashboardIsOpen, audioTrigger
-// anonymousFetchedData (nicht eingeloggt): allEntries, lastWeekEntries, AllActivities
-// userFetchedData (eingeloggt): userEntries, userLastWeekEntries, AllActivities
-// functions: fetchData
-//        handleDashboardIsOpen()
 // Page Routing: const { page } = router.query; (https://mood-tracker.com/smiletrainer)
