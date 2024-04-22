90f0fca7ced459be66dc16276733130cbfcbba7d
Niko Cremer <n.ko@posteo.de>
2024-04-22 13:55:57 +0200

    integrate last week entries

--- components/Dashboard/Tiles/BarChartTile/BarChartTile.js
+++ components/Dashboard/Tiles/BarChartTile/BarChartTile.js
@@ -1,32 +1,54 @@
 import styled from "styled-components";
 import { useSession } from "next-auth/react";
 import experienceAnalyser from "@/utils/experienceAnalyser";
+import lastWeekAnalyser from "@/utils/lastWeekAnalyser";
 import { useData } from "@/lib/useData";
+import { useState } from "react";
 
 export default function BarChartTile() {
   const { data: session } = useSession();
+  const [isLastWeek, setIsLastWeek] = useState(true);
 
   const { allEntries, isLoadingEntries, errorEntries } =
     useData().fetchedAllEntries;
   console.log(allEntries, isLoadingEntries, errorEntries);
-
   const { userEntries } = useData().fetchedUserEntries;
+  const lastWeek = lastWeekAnalyser(session ? userEntries : allEntries);
+  console.log("lastWeek", lastWeek);
 
-  const visualizedData = experienceAnalyser(session ? userEntries : allEntries);
+  const visualizedData = isLastWeek
+    ? experienceAnalyser(lastWeek)
+    : experienceAnalyser(session ? userEntries : allEntries);
+
   const totalCount = visualizedData.totalCount;
   console.log(visualizedData.experiences);
 
   if (isLoadingEntries) return <p>Entries Loading</p>;
   if (errorEntries) return <p>Sorry, there was an error fetching entries</p>;
 
+  function topTwoExperiencesAnalyser(data) {
+    data.sort((a, b) => b.count - a.count);
+    const topTwoExperiences = data.slice(0, 2);
+    return topTwoExperiences;
+  }
+
+  const topTwoExperiences = topTwoExperiencesAnalyser(
+    visualizedData.experiences
+  );
+
+  const emotionFirst = topTwoExperiences[0].experience;
+  const emotionSecond = topTwoExperiences[1].experience;
   return (
     <BarChartContainer>
-      <LastWeekTogglePill>
+      <LastWeekTogglePill
+        onClick={() => {
+          setIsLastWeek(!isLastWeek);
+        }}
+      >
         <span>Last Week</span>
       </LastWeekTogglePill>
       <EntriesDescription>
-        {entriesCount} entries most are {emotionFirst}
-        and {emotionSecond}
+        {totalCount} entries most are {emotionFirst} and {emotionSecond}
       </EntriesDescription>
 
       {visualizedData.experiences &&
