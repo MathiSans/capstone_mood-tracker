39e82be8be35e9fb94b4dfd05da811f7974cb07b
Ramin Faqyri <ramin-faqyri@web.de>
2024-04-24 10:15:35 +0200

    connected with activity-form

--- components/Dashboard/ActivitiesColumn/ActivitiesColumn.js
+++ components/Dashboard/ActivitiesColumn/ActivitiesColumn.js
@@ -1,10 +1,20 @@
 import ActivitiesList from "./ActivitiesList/ActivitiesList.js";
 import { Grid } from "../Dashboard.styled";
+import ActivitiesForm from "./ActivitiesForm/ActivitiesForm.js";
+import { useState } from "react";
 
 export default function ActivitiesColumn() {
+  const [showForm, setShowForm] = useState(false);
+  function handleShowForm() {
+    setShowForm(!showForm);
+  }
   return (
     <Grid>
-      <ActivitiesList />
+      {showForm ? (
+        <ActivitiesForm handleShowForm={handleShowForm} />
+      ) : (
+        <ActivitiesList showForm={showForm} handleShowForm={handleShowForm} />
+      )}
     </Grid>
   );
 }
