c533587790ded79c84c77a3f07413d80754e4e97
Mathis Santos <mail@mathissantos.com>
2024-04-24 09:44:11 +0200

    activity tile build with carousel and dot indicator

---
+++ components/Dashboard/Tiles/ActivityTile/Carousel/Carousel.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import * as Styled from "./Carousel.styled";
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function Carousel({ children }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % children.length);
+    }, 5500);
+
+    return () => clearInterval(interval);
+  }, [children.length]);
+
+  return (
+    <Styled.CarouselContainer>
+      <AnimatePresence initial={false}>
+        {children.map(
+          (child, index) =>
+            index === currentIndex && (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                style={{
+                  position: "absolute",
+                  width: "100%",
+                  top: 0,
+                }}
+              >
+                {child}
+              </motion.div>
+            )
+        )}
+      </AnimatePresence>
+      <Styled.IndicatorContainer>
+        {children.map((_, index) => (
+          <Styled.Indicator key={index} active={index === currentIndex} />
+        ))}
+      </Styled.IndicatorContainer>
+    </Styled.CarouselContainer>
+  );
+}
