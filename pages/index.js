330c8fec16e736a4faa07a06c0eb555953b94b16
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-05-06 12:30:50 +0200

    added logo to temporary index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { Container } from "@/components/Layout/Layout.styled";
 
 export default function Home() {
   const router = useRouter();
@@ -10,5 +11,5 @@ export default function Home() {
     }, 0);
   }, []);
 
-  return null;
+  return <Container>logo</Container>;
 }
