f0ba1168d467b72b70d6ac70aae46f44242d2ae8
Niko Cremer <n.ko@posteo.de>
2024-05-07 10:38:49 +0200

    small typo

--- pages/[path].js
+++ pages/[path].js
@@ -55,7 +55,7 @@ export default function Home() {
   const Component = path?.includes("id:")
     ? Entry
     : path?.includes("tool:")
-    ? ToolsWrapper
+    ? ToolWrapper
     : componentMap[path];
   function handleHideInterface() {
     setHideInterface(!hideInterface);
